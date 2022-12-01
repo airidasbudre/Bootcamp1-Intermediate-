44513181a9ba4a30ee32404baa3761f73d4790b9
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2022-12-01 17:30:52 +0200

    Add files via upload

---
+++ turtle.main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.left(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+my_screen = Screen() 
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
